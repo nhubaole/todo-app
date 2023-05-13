79f6a235cf336fdd7625c9590d36efffb9719f13
nhubaole <okamisameko7123@gmail.com>
2023-05-13 08:45:03 +0700

    add feature creating new todos

--- app/src/main/java/com/example/todoapp/MainActivity.java
+++ app/src/main/java/com/example/todoapp/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.todoapp;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -17,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     ListView lvTodo;
     List<Todo> TodoList = new ArrayList<>();
     TodoAdapter todoAdapter;
+    ActivityResultLauncher<Intent> launcherNewTodo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +31,20 @@ public class MainActivity extends AppCompatActivity {
         TodoList.add(new Todo("Chay deadline", "dl todo app", "17/5/2023", false));
         todoAdapter = new TodoAdapter(this, R.layout.list_item_todo, TodoList);
         lvTodo.setAdapter(todoAdapter);
+
+        launcherNewTodo = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == Activity.RESULT_OK) {
+                            Intent intent = result.getData();
+                            Todo todo = (Todo) intent.getExtras().get("todo");
+                            TodoList.add(todo);
+                            todoAdapter.notifyDataSetChanged();
+                        }
+                    }
+                });
     }
 
     @Override
@@ -36,7 +56,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         if(item.getItemId() == R.id.newOption){
-
+            launcherNewTodo.launch(new Intent(MainActivity.this, NewActivity.class));
         }
         return super.onOptionsItemSelected(item);
     }
---
+++ app/src/main/java/com/example/todoapp/NewActivity.java
@@ -0,0 +1,66 @@
+package com.example.todoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class NewActivity extends AppCompatActivity {
+
+    private EditText dateEdt;
+    private EditText titleEdt;
+    private EditText descEdt;
+    private CheckBox doneCbox;
+    private Button saveBtn;
+    Todo todo;
+    int pos = -1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new);
+        titleEdt = (EditText) findViewById(R.id.titleEdt);
+        descEdt = (EditText) findViewById(R.id.descEdt);
+        doneCbox = (CheckBox) findViewById(R.id.doneCbox);
+        dateEdt = (EditText) findViewById(R.id.dateEdt);
+        dateEdt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar c = Calendar.getInstance();
+                int year = c.get(Calendar.YEAR);
+                int month = c.get(Calendar.MONTH);
+                int day = c.get(Calendar.DAY_OF_MONTH);
+                DatePickerDialog datePickerDialog = new DatePickerDialog(NewActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                        String date = dayOfMonth + "/" + (monthOfYear + 1) + "/" + year;
+                        dateEdt.setText(date);
+                    }
+                }, year, month, day);
+                datePickerDialog.show();
+            }
+        });
+        saveBtn = (Button) findViewById(R.id.saveBtn);
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(NewActivity.this, MainActivity.class);
+                Bundle bundle = new Bundle();
+                todo = new Todo(titleEdt.getText().toString(), descEdt.getText().toString(), dateEdt.getText().toString(), doneCbox.isChecked());
+                bundle.putSerializable("todo", todo);
+                bundle.putInt("position", pos);
+                intent.putExtras(bundle);
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        });
+    }
+}
